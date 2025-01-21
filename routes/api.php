718d94634326c4bac0e1b83678f11f8891e7868d
jivemachine <the.jeremy.cobb@gmail.com>
2025-01-20 20:21:09 -0600

    episode 5 Authentication using tokens

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 
 
 // http://tickets-please.test/api/v1/tickets
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,9 +1,11 @@
 <?php
 
 
+use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\TicketController;
 
 
 
@@ -12,7 +14,7 @@ use App\Http\Controllers\Api\V1\TicketController;
 // tickets
 // users
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 
 
 Route::get('/user', function (Request $request) {

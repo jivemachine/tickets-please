019061cc77cba843d99e036bea580b64ecc55ee9
jivemachine <the.jeremy.cobb@gmail.com>
2025-01-20 18:46:48 -0600

    episode 1 Status 200 code :)

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'Hello, API!'
-    ], 200);
-});
-
+Route::get('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();

bb72930ddba6584dc1c3562d68af4e59a8bf08ed
jivemachine <the.jeremy.cobb@gmail.com>
2025-01-20 19:05:41 -0600

    Episode 2 Working with postman

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login() {
-        return $this->ok('Hello, login!');
+    public function login(ApiLoginRequest $request) {
+        return $this->ok($request->get('email'));
+    }
+
+    public function register() {
+        return $this->ok('register');
     }
 }

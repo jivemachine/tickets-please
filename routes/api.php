79245357e3dd22254fc263d4469792d676eaf16f
jivemachine <the.jeremy.cobb@gmail.com>
2025-01-20 19:17:53 -0600

    Episode 3: Designing URL's

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+
+// http://tickets-please.test/api/
+// universal resource locator
+// tickets
+// users
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();

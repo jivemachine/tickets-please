7a9195e830f8f55381682c903b224e4f6ea7aa87
jivemachine <the.jeremy.cobb@gmail.com>
2025-01-21 18:27:06 -0600

    episode 13: Creating Resources with Post Requests

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Filters\V1\TicketFilter;
+use App\Models\User;
 use App\Models\Ticket;
+use App\Http\Filters\V1\TicketFilter;
+use App\Http\Resources\V1\TicketResource;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
-use App\Http\Resources\V1\TicketResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
@@ -25,7 +27,22 @@ class TicketController extends ApiController
      */
     public function store(StoreTicketRequest $request)
     {
-        //
+        try {
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        } catch (ModelNotFoundException $exception) {
+            return $this->ok('User not found', [
+                'error' => 'The provided user id does not exist'
+            ]);
+        }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $request->input('data.relationships.author.data.id'),
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 
     /**
